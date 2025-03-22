8b3a8358200e7fd2d37590020397032892727508
lulavalenca <lulavalencafilho@gmail.com>
2025-03-21 23:26:16 -0300

    Adicionado exercicio até 30

--- POO/ExercicioPOO1/Program.cs
+++ POO/ExercicioPOO1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{   
+    static void Main()
+    {
+        // Declaração e atribuição de variaveis
+        int numeroInteiro = 42;
+        double numeroDouble = 3.14;
+        char caractere = 'A';
+        bool booleano = true;
+        string texto = "Olá, Mundo!";
+
+        //Exibindo os valores da variaveis na tela 
+        Console.WriteLine("Valor do init: " + numeroInteiro);
+        Console.WriteLine("Valor do double: " + numeroDouble);
+        Console.WriteLine("Valor do char: " + caractere);
+        Console.WriteLine("Valor do bool: " + booleano);
+        Console.WriteLine("Valor do string: " + texto);
+    }
+}
+    
--- POO/ExercicioPOO1/obj/Debug/net9.0/ExercicioPOO1.AssemblyInfo.cs
+++ POO/ExercicioPOO1/obj/Debug/net9.0/ExercicioPOO1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExercicioPOO1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5489cd13bc935922676a24895795a8b0114143fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExercicioPOO1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExercicioPOO1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
