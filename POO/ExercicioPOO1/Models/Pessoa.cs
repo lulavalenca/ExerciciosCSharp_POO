1b76a00ac7172033412df57ff366aab9b8040caa
lulavalenca <lulavalencafilho@gmail.com>
2025-03-22 09:57:57 -0300

    Adicionando Exercicios de POO e pasta de arquivos

---
+++ POO/ExercicioPOO1/Models/Pessoa.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExercicioPOO1.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
--- SintaxeBasica/Exercicio1/obj/Debug/net9.0/Exercicio1.AssemblyInfo.cs
+++ SintaxeBasica/Exercicio1/obj/Debug/net9.0/Exercicio1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b3a8358200e7fd2d37590020397032892727508")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
